d88e6a860262e1c25d832cb738907be879733abc
Matěj Pekár <525077@mail.muni.cz>
2025-11-04 00:57:56 +0100

    feat: submit job

--- submit.sh
+++ submit.sh
@@ -3,11 +3,18 @@
 # SLURM SUBMIT SCRIPT
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=16
+#SBATCH --cpu-bind=none
 #SBATCH --gres=gpu:4            # Request N GPUs per machine
 #SBATCH --mem=0
-#SBATCH --time=0-02:00:00
+#SBATCH -D /project/project_465002057/nuclei-foundational-model
+#SBATCH --time=0-12:00:00
+#SBATCH --account=project_465002057
+#SBATCH --partition=standard-g
 
-# Activate conda environment
-source activate $1
+export MLFLOW_TRACKING_URI="file:///scratch/project_465002057/mlruns"
 
-srun uv run python -m nfm +trainer.num_nodes=$SLURM_NNODES +trainer.devices=$SLURM_GPUS_PER_NODE +trainer.strategy=deepspeed_stage_2
+srun singularity exec \
+    -B "/flash/project_465002057,/scratch/project_465002057,/projappl/project_465002057" \
+    "/project/project_465002057/nfm_0.4.0-rocm.sif" \
+    python3 -m nfm mode=fit +experiment=LUMI +trainer.num_nodes=$SLURM_NNODES +trainer.devices=$SLURM_GPUS_ON_NODE +trainer.strategy=deepspeed_stage_2
