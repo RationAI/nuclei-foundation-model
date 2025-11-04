e59930e456174298a4249921c316412f106cd427
Matěj Pekár <525077@mail.muni.cz>
2025-11-04 12:00:44 +0100

    fix: job submit script

--- submit.sh
+++ submit.sh
@@ -2,18 +2,34 @@
 
 # SLURM SUBMIT SCRIPT
 #SBATCH --nodes=4
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=16
-#SBATCH --gres=gpu:4            # Request N GPUs per machine
-#SBATCH --mem=0
+#SBATCH --ntasks-per-node=8
+#SBATCH --gpus-per-node=8       # Request N GPUs per machine
+#SBATCH --mem=0                  # Request all memory
 #SBATCH -D /project/project_465002057/nuclei-foundational-model
 #SBATCH --time=0-12:00:00
 #SBATCH --account=project_465002057
 #SBATCH --partition=standard-g
 
-export MLFLOW_TRACKING_URI="file:///scratch/project_465002057/mlruns"
+cat << EOF > select_gpu
+#!/bin/bash
 
-srun singularity exec \
+export ROCR_VISIBLE_DEVICES=\$SLURM_LOCALID
+exec \$*
+EOF
+
+chmod +x ./select_gpu
+
+CPU_BIND="mask_cpu:7e000000000000,7e00000000000000"
+CPU_BIND="${CPU_BIND},7e0000,7e000000"
+CPU_BIND="${CPU_BIND},7e,7e00"
+CPU_BIND="${CPU_BIND},7e00000000,7e0000000000"
+
+export MLFLOW_TRACKING_URI="file:///scratch/project_465002057/mlruns"
+export MPICH_GPU_SUPPORT_ENABLED=1
+
+srun --cpu-bind=${CPU_BIND} ./select_gpu singularity exec \
     -B "/flash/project_465002057,/scratch/project_465002057,/projappl/project_465002057" \
     "/project/project_465002057/nfm_0.4.0-rocm.sif" \
-    python3 -m nfm mode=fit +experiment=LUMI +trainer.num_nodes=$SLURM_NNODES +trainer.devices=$SLURM_GPUS_ON_NODE +trainer.strategy=deepspeed_stage_2
+    python3 -m nfm mode=fit +experiment=LUMI +trainer.num_nodes=$SLURM_NNODES +trainer.devices=$SLURM_GPUS_ON_NODE +trainer.strategy=deepspeed_stage_2 data.num_workers=6
+
+rm -rf ./select_gpu
