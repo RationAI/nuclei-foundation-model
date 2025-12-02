42f5d6772e2a20466ed9c054e25cd26c52682ee0
Matěj Pekár <525077@mail.muni.cz>
2025-12-02 21:17:09 +0100

    fix: run script

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@
 #SBATCH --gpus-per-node=8       # Request N GPUs per machine
 #SBATCH --mem=0                  # Request all memory
 #SBATCH -D /project/project_465002057/nuclei-foundational-model
-#SBATCH --time=0-12:00:00
+#SBATCH --time=0-24:00:00
 #SBATCH --account=project_465002057
 #SBATCH --partition=standard-g
 
@@ -18,13 +18,7 @@ CPU_BIND="${CPU_BIND},7e00000000,7e0000000000"
 export MLFLOW_TRACKING_URI="file:///scratch/project_465002057/mlruns"
 export MPICH_GPU_SUPPORT_ENABLED=1
 
-# LUMI-O S3 Credentials
-# Generate these at https://auth.lumidata.eu/
-export AWS_ACCESS_KEY_ID="<YOUR_ACCESS_KEY_ID>"
-export AWS_SECRET_ACCESS_KEY="<YOUR_SECRET_ACCESS_KEY>"
-export AWS_ENDPOINT_URL="https://lumidata.eu"
-
 srun --cpu-bind=${CPU_BIND} singularity exec \
-    -B "/scratch/project_465002057,/projappl/project_465002057" \
-    "/project/project_465002057/nfm_0.5.0-rocm.sif" \
+    -B "/flash/project_465002057,/scratch/project_465002057,/projappl/project_465002057" \
+    "/project/project_465002057/nfm_0.6.0-rocm.sif" \
     python3 -m nfm mode=fit +experiment=LUMI +trainer.num_nodes=$SLURM_NNODES +trainer.devices=$SLURM_GPUS_ON_NODE +trainer.strategy=deepspeed_stage_2
