fdb4a31666def7c9416abf38c6b74bb40272170f
Matěj Pekár <525077@mail.muni.cz>
2025-12-02 20:56:37 +0100

    fix: import

--- nfm/modeling/layers/__init__.py
+++ nfm/modeling/layers/__init__.py
@@ -1,5 +1,4 @@
 from nfm.modeling.layers.ffn import FeedForward
-from nfm.modeling.layers.mlp import MLP
 from nfm.modeling.layers.rope import RoPE
 
-__all__ = ["FeedForward", "MLP", "RoPE"]
+__all__ = ["FeedForward", "RoPE"]
