f2aecf9243eb5382c2b300e323ba3abe98cd8708
Matěj Pekár <matej.pekar120@gmail.com>
2026-05-01 19:33:40 +0200

    fix: encoding

--- nfm/modeling/transformer.py
+++ nfm/modeling/transformer.py
@@ -108,5 +108,5 @@ class NFM(nn.Module):
         normalized[:seq_len] = self.bn(x[:seq_len])
         x = normalized
 
-        x = x + self.position_encoder(pos / 50000)
+        x = self.polygon_proj(x) + self.position_encoder(pos / 10000)
         return self.backbone(x[None], pos[None], block_mask).squeeze(0)
